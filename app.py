627cc67c0965a9123357f18637732506a579ed9a
Hüseyin Evecen <36777104+huseyinevecen@users.noreply.github.com>
2023-01-31 15:05:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ itos = { cantor_pair(i, len(chars)):ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
-print(encode("hii there"))
-print(decode(encode("hii there")))
+print(encode("Any consistent formal system F within which a certain amount of elementary arithmetic can be carried out is incomplete; i.e., there are statements of the language of F which can neither be proved nor disproved in F."))
+print(decode(encode("Any consistent formal system F within which a certain amount of elementary arithmetic can be carried out is incomplete; i.e., there are statements of the language of F which can neither be proved nor disproved in F.")))
